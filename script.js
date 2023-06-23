ffa813a838b39c5aec6c2e09a23b4e743b9871ad
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-23 20:47:25 +0300

    dice-game assignment -Done

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 
 
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
 
 
 let scores, currentScore, activePlayer, playing;
@@ -44,7 +46,6 @@ const switchPlayer = function () {
 
 
 
-
 btnRoll.addEventListener('click', function () {
   if (playing) {
     
@@ -67,27 +68,28 @@ btnRoll.addEventListener('click', function () {
 });
 
 
+const winningScore = 100;
+
+
 btnHold.addEventListener('click', function () {
   if (playing) {
     
-
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
     
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= winningScore) {
       
-
       playing = false;
       diceEl.classList.add('hidden');
       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+      document.querySelector(`#name--${activePlayer}`).textContent = 'Winner!';
       document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
     } else {
-      
+     
       switchPlayer();
     }
   }
 });
-
 
 btnNew.addEventListener('click', init);
